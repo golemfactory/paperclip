ca9b8a01ab260a01755fd7d062b88d9ee2d9aff4
Michal Pokrywka <wolfmoon@o2.pl>
2021-12-13 18:12:21 +0100

    actix-(multipart,files,session) v4 support

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -19,3 +19,30 @@ pub mod v2;
 pub mod v3;
 
 pub use self::error::ValidationError;
+
+#[cfg(all(feature = "actix2", feature = "actix3"))]
+compile_error!("feature \"actix2\" and feature \"actix3\" cannot be enabled at the same time");
+
+#[cfg(all(feature = "actix3", feature = "actix4"))]
+compile_error!("feature \"\" and feature \"\" cannot be enabled at the same time");
+
+#[cfg(all(feature = "actix2", feature = "actix4"))]
+compile_error!("feature \"\" and feature \"\" cannot be enabled at the same time");
+
+#[cfg(all(feature = "actix-multipart", any(feature = "actix2", feature = "actix3")))]
+compile_error!("feature \"actix-multipart\" should go together with feature \"actix\" or \"actix4\"");
+
+#[cfg(all(feature = "actix-session", any(feature = "actix2", feature = "actix3")))]
+compile_error!("feature \"actix-session\" should go together with feature \"actix\" or \"actix4\"");
+
+#[cfg(all(feature = "actix-files", any(feature = "actix2", feature = "actix3")))]
+compile_error!("feature \"actix-files\" should go together with feature \"actix\" or \"actix4\"");
+
+#[cfg(all(feature = "actix-multipart3", feature = "actix4"))]
+compile_error!("feature \"actix-multipart3\" should go together with feature \"actix2\" or \"actix3\"");
+
+#[cfg(all(feature = "actix-session3", feature = "actix4"))]
+compile_error!("feature \"actix-session3\" should go together with feature \"actix2\" or \"actix3\"");
+
+#[cfg(all(feature = "actix-files3", feature = "actix4"))]
+compile_error!("feature \"actix-files3\" should go together with feature \"actix2\" or \"actix3\"");
